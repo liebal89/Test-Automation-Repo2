51f514257756fe32755209cf95afb442e707ebdf
liebal89 <56328127+liebal89@users.noreply.github.com>
2020-03-06 14:18:49 +0000

    Update login_logout.bat

--- Zephyr zBot Scripts/TestExecute/login_logout.bat
+++ Zephyr zBot Scripts/TestExecute/login_logout.bat
@@ -1,5 +1,6 @@
 set HOME=%USERPROFILE%
 cd "C:\Users\Administrator\Desktop\Test Automation Repo\Test-Automation-Repo2"
+git clean -df
 git fetch
 git pull
 cd "C:\Program Files (x86)\SmartBear\TestExecute 14\x64\Bin\"
