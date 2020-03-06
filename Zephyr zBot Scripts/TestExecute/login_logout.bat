900e12edf6e78271d7e7413a537d728aee2b2704
liebal89 <56328127+liebal89@users.noreply.github.com>
2020-03-06 14:16:25 +0000

    Update login_logout.bat

--- Zephyr zBot Scripts/TestExecute/login_logout.bat
+++ Zephyr zBot Scripts/TestExecute/login_logout.bat
@@ -5,6 +5,7 @@ git pull
 cd "C:\Program Files (x86)\SmartBear\TestExecute 14\x64\Bin\"
 TestExecute.exe "C:\Users\Administrator\Desktop\Test Automation Repo\Test-Automation-Repo2\TestComplete Tests\easyTravel Test Automation.pjs" /r /e /p:"Login_Logout" /ExportSummary:"C:\Users\Administrator\Desktop\Test Results\Login Logout\results.xml"
 taskkill /IM firefox.exe /F
+git clean -df
 
 
 
