d0a81dfe9ea4b5ddfbd40002119a44c4d135a56b
liebal89 <56328127+liebal89@users.noreply.github.com>
2020-03-06 13:57:25 +0000

    test

--- Zephyr zBot Scripts/TestExecute/login_logout.bat
+++ Zephyr zBot Scripts/TestExecute/login_logout.bat
@@ -1,5 +1,6 @@
 set HOME=%USERPROFILE%
 cd "C:\Users\Administrator\Desktop\Test Automation Repo\Test-Automation-Repo2"
+git fetch
 git pull
 cd "C:\Program Files (x86)\SmartBear\TestExecute 14\x64\Bin\"
 TestExecute.exe "C:\Users\Administrator\Desktop\Test Automation Repo\Test-Automation-Repo2\TestComplete Tests\easyTravel Test Automation.pjs" /r /e /p:"Login_Logout" /ExportSummary:"C:\Users\Administrator\Desktop\Test Results\Login Logout\results.xml"
