de26a6d278589a7cf09c18befebac99882435a33
liebal89 <56328127+liebal89@users.noreply.github.com>
2020-03-06 14:40:11 +0000

    updated zbot scripts

--- Zephyr zBot Scripts/TestExecute/contact_us_larger_map.bat
+++ Zephyr zBot Scripts/TestExecute/contact_us_larger_map.bat
@@ -1,7 +1,10 @@
 set HOME=%USERPROFILE%
-cd "C:\Users\Administrator\Desktop\Test_Automation_Repo"
+cd "C:\Users\Administrator\Desktop\Test Automation Repo\Test-Automation-Repo2"
+git clean -df
+git fetch
 git pull
 cd "C:\Program Files (x86)\SmartBear\TestExecute 14\x64\Bin\"
-TestExecute.exe "C:\Users\Administrator\Desktop\Test_Automation_Repo\TestComplete Tests\easyTravel Test Automation.pjs" /r /e /p:"Contact_Us_Larger_Map" /ExportSummary:"C:\Users\Administrator\Desktop\Test_Automation_Repo\Test Results\Contact Us\results.xml"
-
+TestExecute.exe "C:\Users\Administrator\Desktop\Test Automation Repo\Test-Automation-Repo2\TestComplete Tests\easyTravel Test Automation.pjs" /r /e /p:"Contact_Us_Larger_Map" /ExportSummary:"C:\Users\Administrator\Desktop\Test Results\Contact Us\results.xml"
+taskkill /IM firefox.exe /F
+git clean -df
 
--- Zephyr zBot Scripts/TestExecute/terms_and_privacy.bat
+++ Zephyr zBot Scripts/TestExecute/terms_and_privacy.bat
@@ -1,6 +1,9 @@
 set HOME=%USERPROFILE%
-cd "C:\Users\Administrator\Desktop\Test_Automation_Repo"
+cd "C:\Users\Administrator\Desktop\Test Automation Repo\Test-Automation-Repo2"
+git clean -df
+git fetch
 git pull
-cd "C:\Program Files (x86)\SmartBear\TestExecute 14\x64\Bin\"
-TestExecute.exe "C:\Users\Administrator\Desktop\Test_Automation_Repo\TestComplete Tests\easyTravel Test Automation.pjs" /r /e /p:"Terms_and_Privacy" /ExportSummary:"C:\Users\Administrator\Desktop\Test_Automation_Repo\Test Results\Terms and Privacy\results.xml"
-
+ccd "C:\Program Files (x86)\SmartBear\TestExecute 14\x64\Bin\"
+TestExecute.exe "C:\Users\Administrator\Desktop\Test Automation Repo\Test-Automation-Repo2\TestComplete Tests\easyTravel Test Automation.pjs" /r /e /p:"Terms_and_Privacy" /ExportSummary:"C:\Users\Administrator\Desktop\Test Results\Terms and Privacy\results.xml"
+taskkill /IM firefox.exe /F
+git clean -df
