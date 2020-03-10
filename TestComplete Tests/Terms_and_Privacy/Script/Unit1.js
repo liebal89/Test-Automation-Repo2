a8fed938751e3f75264b4c1946e8906f34a01286
liebal89 <liebal89@gmail.com>
2020-03-10 18:33:20 +0000

    update scripts

--- TestComplete Tests/Terms_and_Privacy/Script/Unit1.js
+++ TestComplete Tests/Terms_and_Privacy/Script/Unit1.js
@@ -1,1 +1,26 @@
-﻿
+﻿function contact_us_larger_map()
+{
+  //Launches the specified browser and opens the specified URL in it.
+  Browsers.Item(btFirefox).Run("about:blank");
+  //Opens the specified URL in a running instance of the specified browser.
+  Browsers.Item(btFirefox).Navigate("http://54.245.179.85:8080/");
+  //Create and send an HTTP request with the specified method
+  var request, response, LastResult, request1, response1;
+  request = aqHttp.CreateRequest("HEAD", "http://54.245.179.85.8080", "", "");
+  request.SetHeader("x-dynatrace-test", "contact us");
+  response = request.Send(undefined);
+  LastResult = response;
+  //Clicks the 'linkContact' control.
+  Aliases.browser.pageEasytravelOneStepToHappiness2.linkContact.Click();
+  //Create and send an HTTP request with the specified method
+  request1 = aqHttp.CreateRequest("HEAD", "http://54.245.179.85.8080", "", "");
+  request1.SetHeader("x-dynatrace-test", "contact us");
+  response1 = request1.Send(undefined);
+  LastResult = response1;
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageEasytravelContact2.Wait();
+  //Clicks the 'linkViewOnLargerMap' control.
+  Aliases.browser.pageEasytravelContact2.formIceform.linkViewOnLargerMap.Click();
+  //Waits until the browser loads the page and is ready to accept user input.
+  Aliases.browser.pageGoogleMaps.Wait();
+}
---
+++ TestComplete Tests/Terms_and_Privacy/Script/contact_us_script.js
@@ -0,0 +1,26 @@
+﻿
+  
+  function httpGetRequest()
+{
+  var address = "http://54.245.179.85:8079";
+
+  // Create an aqHttpRequest object
+  var aqHttpRequest = aqHttp.CreateGetRequest(address);
+
+  // Specify the Content-Type header value
+  aqHttpRequest.SetHeader("x-dynatrace-functional-test", "LSN1: Report an Outage; TSN1: Loading of Homepage");
+  aqHttpRequest.SetHeader("x-dynatrace-functional-test", "LSN1: Report an Outage; TSN2: Loading Outage Map");
+
+  // Send the request, get an aqHttpResponse object
+  var aqHttpResponse = aqHttpRequest.Send();
+  
+  // Read the response data
+  Log.Message(aqHttpResponse.AllHeaders); // All headers
+  Log.Message(aqHttpResponse.GetHeader("x-dynatrace-functional-test")); // A specific header
+  Log.Message(aqHttpResponse.StatusCode); // A status code
+  Log.Message(aqHttpResponse.StatusText); // A status text
+  Log.Message(aqHttpResponse.Text); // A response body
+  
+  // Save a response body to a file
+  aqHttpResponse.SaveToFile("C:\\Work\\body.txt");
+}
